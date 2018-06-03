5d6601ff448329beacb321d66d731bf0e9dd1b42
飯塚 孝高 <iizukayoshitaka@YoshiMac.local>
2018-06-03 13:41:43 +0900

    carousel

--- app.py
+++ app.py
@@ -562,7 +562,7 @@ def handle_text_message(event):
     elif message_pattern == 'carousel':
         carousel_template = CarouselTemplate(columns=[
             CarouselColumn(
-                thumbnailImageUrl='https://nekobot-line.herokuapp.com/static/restaurantimg/zoot.jpg',
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='ラーメン、居酒屋、焼きとん\n'+'営業時間:17:00～24:00',
                 title='ZOOT [浜松町]',
                 actions=[
@@ -572,7 +572,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='牛タン、麦とろ、カレーライス\n'+'営業時間:17:30～22:30',
                 title='利助 [浜松町]',
                 actions=[
@@ -582,7 +582,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='中華料理、中国鍋・火鍋、ラーメン\n'+'営業時間:17:00～23:00(L.O. 22:30)',
                 title='青蓮 [浜松町]',
                 actions=[
@@ -592,7 +592,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='魚介料理・海鮮料理、居酒屋\n'+'営業時間:17:00～23:30',
                 title='魚金 [浜松町]',
                 actions=[
@@ -602,7 +602,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='ダイニングバー、ワインバー\n'+'営業時間:17:00～23:30(L.O.22:30、ドリンクL.O.23:00)',
                 title='bonanza [浜松町]',
                 actions=[
@@ -612,7 +612,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='王様のブランチ第２位の餃子\n'+'営業時間:17:00～23:00(L.O.22:20)',
                 title='東海飯店 [浜松町]',
                 actions=[
@@ -622,7 +622,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='ビストロ、イタリアン、居酒屋\n'+'営業時間:17:00～23:30(L.O.22:45)',
                 title='ビストロガブリ [浜松町]',
                 actions=[
@@ -632,7 +632,7 @@ def handle_text_message(event):
             ]),
 
             CarouselColumn(
-                thumbnailImageUrl=restaurant_image_url('zoot'),
+                thumbnail_image_url=restaurant_image_url('zoot'),
                 text='居酒屋、くじら料理\n'+'営業時間:16:30～23:30',
                 title='旨蔵 うま八 [新橋]',
                 actions=[
