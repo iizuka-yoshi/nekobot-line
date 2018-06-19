8c3211dfb22b0f5cb750b2098445475a7f9024d5
飯塚 孝高 <iizukayoshitaka@YoshiMac.local>
2018-06-19 23:07:53 +0900

    db entity

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def shrink_image(source_path,save_path, target_width, target_height):
 
 def get_entitｙ(text):
 
-    sql = 'SELECT name FROM entities \"' + text + '\" = ANY (synonym);'
+    sql = 'SELECT name FROM entities \'にゃんこ\' = ANY (synonym);'
 
     with psycopg2.connect(DB_URL) as conn:
         with conn.cursor() as curs:
