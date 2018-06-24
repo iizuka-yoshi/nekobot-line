e3294d19b4a4ca947ba075dcdf0f533d67a17b4d
飯塚 孝高 <iizukayoshitaka@YoshiMac.local>
2018-06-24 23:53:05 +0900

    db

--- app.py
+++ app.py
@@ -288,8 +288,7 @@ def text_send_messages_db(entity):
 
     messages = []
     for reply_text in reply_texts:
-        print('reply_text '+ reply_text)
-        messages.append(ImageSendMessage(text=reply_text))
+        messages.append(TextSendMessage(text=reply_text))
 
     return messages
 
