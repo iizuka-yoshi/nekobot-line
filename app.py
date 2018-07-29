22fe40126c601ff88ee77d7f79f8d79bf79e43fb
飯塚 孝高 <iizukayoshitaka@YoshiMac.local>
2018-07-29 21:27:18 +0900

    tabelog class

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ class _Tabelog_Insert:
 
     def _normalize_hours(self, hours):
         hoursn = hours
-        hoursn = hoursn.replace('〜','-')
+        hoursn = hoursn.replace('～','-')
         hoursn = neologdn.normalize(hoursn)
         return hoursn
     
