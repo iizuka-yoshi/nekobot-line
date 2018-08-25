fb6613782e7202541277f1bf1501403dd1042445
飯塚 孝高 <iizukayoshitaka@YoshiMac.local>
2018-08-25 22:54:35 +0900

    godrink

--- app.py
+++ app.py
@@ -1030,7 +1030,7 @@ def handle_text_message(event):
                     template=CarouselTemplate(columns=t_select.carousel_columns())
                 )
 
-                replies = text_send_messages_db(entity_exact) + template_message
+                replies = text_send_messages_db(entity_exact).append(template_message)
                 line_bot_api.reply_message(event.reply_token,replies)
 
                 return
